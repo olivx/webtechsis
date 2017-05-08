e3c11e0108d82b18d7a8f9c3e870db4df548a67d
olivx <oliveiravicente.net@gmail.com>
2017-05-08 16:56:10 -0300

    bootstrap validtor para form boletos

--- faturamento/views/views_boletos.py
+++ faturamento/views/views_boletos.py
@@ -63,56 +63,53 @@ def boleto_midia_detail(request, nnum_bol):
 
 
 def boleto_update(request, nnum_bol):
-    empresa = request.POST.get('empresa')
+    if request.POST:
+        empresa = request.POST.get('empresa')
+        # verifica de qual empresa é para montar o form
+        if empresa == 'TechCD':
+            boleto = get_object_or_404(BoletoTech.objects.using('techcd'), nnum_bol=nnum_bol)
+            form = BoletoTechForm(request.POST, instance=boleto)
+            if boleto.ind_tip_nfe_srv:
+                item_pag = get_object_or_404(ItemNfeSaidaPagtoTechSRV.objects.using('techcd'),
+                                             cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
+            else:
 
-    # verifica de qual empresa é para montar o form
-    if empresa == 'TechCD':
-        boleto = get_object_or_404(BoletoTech.objects.using('techcd'), nnum_bol=nnum_bol)
-        form = BoletoTechForm(request.POST, instance=boleto)
-        if boleto.ind_tip_nfe_srv:
-            item_pag = get_object_or_404(ItemNfeSaidaPagtoTechSRV.objects.using('techcd'),
-                                         cod_nf_saida=boleto.numdoc_bol , datavencnf_saida_pagto=boleto.venc_bol)
+                item_pag = get_object_or_404(ItemNfeSaidaPagtoTech.objects.using('techcd'),
+                                             cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
+
+
+        elif empresa == 'Data Store':
+            boleto = get_object_or_404(BoletoData.objects.using('techcd'), nnum_bol=nnum_bol)
+            form = BoletoDataForm(request.POST, instance=boleto)
+            if boleto.ind_tip_nfe_srv:
+                item_pag = get_object_or_404(ItemNfeSaidaPagtoDataSRV.objects.using('techcd'),
+                                             cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
+            else:
+
+                item_pag = get_object_or_404(ItemNfeSaidaPagtoData.objects.using('techcd'),
+                                             cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
         else:
+            boleto = get_object_or_404(BoletoMidia.objects.using('techcd'), nnum_bol=nnum_bol)
+            form = BoletoDataForm(request.POST, instance=boleto)
+            if boleto.ind_tip_nfe_srv:
+                item_pag = get_object_or_404(ItemNfeSaidaPagtoMidiaSRV.objects.using('techcd'),
+                                             cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
+            else:
 
-            item_pag = get_object_or_404(ItemNfeSaidaPagtoTech.objects.using('techcd'),
-                                         cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
+                item_pag = get_object_or_404(ItemNfeSaidaPagtoMidia.objects.using('techcd'),
+                                             cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
 
+                # salva o boleto
+        if form.is_valid():
+            bol = form.save()
 
-    elif empresa == 'Data Store':
-        boleto = get_object_or_404(BoletoData.objects.using('techcd'), nnum_bol=nnum_bol)
-        form = BoletoDataForm(request.POST, instance=boleto)
-        if boleto.ind_tip_nfe_srv:
-            item_pag = get_object_or_404(ItemNfeSaidaPagtoDataSRV.objects.using('techcd'),
-                                         cod_nf_saida=boleto.numdoc_bol , datavencnf_saida_pagto=boleto.venc_bol)
+            _instance = type(item_pag)
+            _instance.objects.using('techcd'). \
+                filter(Q(cod_nf_saida=item_pag.cod_nf_saida) &
+                       Q(datavencnf_saida_pagto=item_pag.datavencnf_saida_pagto)). \
+                update(datavencnf_saida_pagto=bol.venc_bol, valnf_saida_pagto=bol.val_bol)
+            messages.success(request, 'NFe {0}  da  {1}  Alterada  com Sucesso!'.format(boleto.numdoc_bol, empresa))
         else:
-
-            item_pag = get_object_or_404(ItemNfeSaidaPagtoData.objects.using('techcd'),
-                                         cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
-    else:
-        boleto = get_object_or_404(BoletoMidia.objects.using('techcd'), nnum_bol=nnum_bol)
-        form = BoletoDataForm(request.POST, instance=boleto)
-        if boleto.ind_tip_nfe_srv:
-            item_pag = get_object_or_404(ItemNfeSaidaPagtoMidiaSRV.objects.using('techcd'),
-                                         cod_nf_saida=boleto.numdoc_bol , datavencnf_saida_pagto=boleto.venc_bol)
-        else:
-
-            item_pag = get_object_or_404(ItemNfeSaidaPagtoMidia.objects.using('techcd'),
-                                         cod_nf_saida=boleto.numdoc_bol, datavencnf_saida_pagto=boleto.venc_bol)
-
-        # salva o boleto
-    if form.is_valid():
-        bol = form.save()
-        # item_pag.datavencnf_saida_pagto = bol.venc_bol
-
-        _instance  =  type(item_pag)
-        _instance.objects.using('techcd').\
-            filter(Q(cod_nf_saida=item_pag.cod_nf_saida) &
-                   Q(datavencnf_saida_pagto=item_pag.datavencnf_saida_pagto)).\
-            update(datavencnf_saida_pagto=bol.venc_bol, valnf_saida_pagto=bol.val_bol)
-
-
-        messages.success(request, 'NFe {0}  da  {1}  Alterada  com Sucesso!'.format(boleto.numdoc_bol, empresa))
-
-        # atulizado o item de pagamento
+            form = BoletoTechForm(request.POST)
 
     return render(request, 'faturamento/boleto.html', {'form': form})
